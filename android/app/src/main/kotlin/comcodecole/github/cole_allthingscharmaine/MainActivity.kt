57a0c7a0f8bbf768db21998c79b95d6b5eb050c5
github.io/codecole <cole.iceneil@gmail.com>
2019-11-26 19:42:53 +0100

    UI screens implemented

---
+++ android/app/src/main/kotlin/comcodecole/github/cole_allthingscharmaine/MainActivity.kt
@@ -0,0 +1,13 @@
+package comcodecole.github.cole_allthingscharmaine
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
